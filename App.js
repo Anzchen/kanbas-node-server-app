c343efc6a686d8fe1366c9306c3d064cef6ba16d
Anzchen <Anthonyzchen@yahoo.com>
2024-04-16 22:23:12 -0400

    Update App.js

--- App.js
+++ App.js
@@ -7,8 +7,8 @@ import mongoose from "mongoose";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import session from "express-session";
 import "dotenv/config";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
-mongoose.connect(CONNECTION_STRING);
+
+mongoose.connect(process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/kanbas");
 const app = express();
 app.use(
     cors({
@@ -16,34 +16,20 @@ app.use(
         origin: process.env.FRONTEND_URL,
     })
 );
-// const sessionOptions = {
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-//   };
-//   if (process.env.NODE_ENV !== "development") {
-//     sessionOptions.proxy = true;
-//     sessionOptions.cookie = {
-//       sameSite: "none",
-//       secure: true,
-//       domain: process.env.HTTP_SERVER_DOMAIN,
-//     };
-//   }
-// app.use(session(sessionOptions));  
-
-app.use(
-    session({
+const sessionOptions = {
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    proxy: true,
-    cookie: {
-        sameSite: "none",
-        secure: true,
-        domain: process.env.HTTP_SERVER_DOMAIN,
-        },
-    })
-);
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+  }
+app.use(session(sessionOptions));  
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
