77008cf22ea85d338b1ed85ea94a96666a5d9b5d
Anzchen <Anthonyzchen@yahoo.com>
2024-04-16 22:36:18 -0400

    Update App.js

--- App.js
+++ App.js
@@ -7,19 +7,19 @@ import mongoose from "mongoose";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import session from "express-session";
 import "dotenv/config";
-
+mongoose.connect(process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/kanbas");
+const app = express();
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 }
+
 // app.configure(function() {
-    app.use(allowCrossDomain);
 // });    
 
-mongoose.connect(process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/kanbas");
-const app = express();
+app.use(allowCrossDomain);
 app.use(
     cors({
         credentials: true,
