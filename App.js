2d903740afc65832bc69603a82add4f3c9c95bd7
Anzchen <Anthonyzchen@yahoo.com>
2024-04-16 22:33:29 -0400

    fix cors?

--- App.js
+++ App.js
@@ -8,6 +8,16 @@ import UserRoutes from "./Kanbas/Users/routes.js";
 import session from "express-session";
 import "dotenv/config";
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+// app.configure(function() {
+    app.use(allowCrossDomain);
+// });    
+
 mongoose.connect(process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/kanbas");
 const app = express();
 app.use(
